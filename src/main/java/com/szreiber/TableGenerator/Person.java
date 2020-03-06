358c62c79d4700d28dfcce5ba2fda927c75dc491
Arkadiusz Szreiber <szreiber.arkadiusz@gmail.com>
2020-03-06 16:51:12 +0100

    removed unused hibernate annotations

--- src/main/java/com/szreiber/TableGenerator/Person.java
+++ src/main/java/com/szreiber/TableGenerator/Person.java
@@ -2,23 +2,13 @@ package com.szreiber.TableGenerator;
 
 import lombok.Data;
 
-import javax.persistence.Column;
-import javax.persistence.Id;
 import java.time.LocalDate;
 
 @Data
 public class Person {
 
-    @Id
-    private Long id;
-
-    @Column
     private String firstName;
-
-    @Column
     private String lastName;
-
-    @Column
     private LocalDate birthday;
 
     public Person() {
